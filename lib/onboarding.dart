6ebf220c9b6ab6fc5c3353351e39e3ca1549b809
adnan ahmed <adnanahmed504423@gmail.com>
2025-09-03 18:40:33 +0530

    onboarding screen

--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -6,32 +6,59 @@ class Onboarding extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff2C2B34),
       body: Column(
         children: [
-          Container(
-            height: 100,
-            width: 150,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/onboarding.png'),
-                fit: BoxFit.cover,
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/onboarding.png'),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
           ),
-          Container(
-            child: Column(
-              children: [
-                Text(
-                  "Premium Cars. \nFollow the luxory",
-                  style: TextStyle(
-                    color: Colors.black54,
-                    fontSize: 35,
-                    fontWeight: FontWeight.w500,
+          Expanded(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Premium Cars. \nFollow the luxory",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                SizedBox(height: 10),
-                Text("Premium and practice car daily rental"),
-              ],
+                  SizedBox(height: 10),
+                  Text(
+                    "Premium and practice car daily rental\nExperience the thrill at a lower price",
+                    style: TextStyle(fontSize: 16, color: Colors.grey),
+                  ),
+                  SizedBox(height: 20),
+                  SizedBox(
+                    width: 319,
+                    height: 54,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.white,
+                      ),
+                      child: Text(
+                        "Get Started",
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
